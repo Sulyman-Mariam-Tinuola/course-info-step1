d85459769bbe44a3742302ea9810ac1ff8c147eb
Sulyman Mariam Tinuola <sulymanmariam88@gmail.com>
2022-06-28 16:44:20 +0100

    course info

--- src/App.js
+++ src/App.js
@@ -1,25 +1,145 @@
-import logo from './logo.svg';
-import './App.css';
+// const App = () => {
+//   const course = {
+//     id: 1,
+//     name: 'Half Stack application development',
+//     parts: [
+//       {
+//         name: 'Fundamentals of React',
+//         e]xercises: 10,
+//         id: 1
+//       },
+//       {
+//         name: 'Using props to pass data',
+//         exercises: 7,
+//         id: 2
+//       },
+//       {
+//         name: 'State of a component',
+//         exercises: 14,
+//         id: 3
+//       }
+//     ]
+// }
 
-function App() {
+//   const Part = (props) =>  (
+//     <div>
+//       {props.name} {props.exercises}
+//     </div>
+//   )
+
+//   function Content () {
+//     return (
+//   <div>
+//   <Part name={course.parts[0].name} exercises={course.parts[0].exercises}/>
+//   <Part name={course.parts[1].name} exercises={course.parts[1].exercises}/>
+//   <Part name={course.parts[2].name} exercises={course.parts[2].exercises}/>
+//   </div> )}
+
+// const Header = () => (
+// <h1>{course.name}</h1> )
+
+//   const Course = () => (
+//     <div>
+//     <Header/>
+//     <Content/>
+//     </div>
+//     )
+
+//   return <Course course={course} />
+// }
+
+// export default App
+
+// const Header = (props)=> {
+//   return (
+//     <div>
+//       <p>Header {props.course} {props.exercises}</p>
+//     </div>
+//   )
+// }
+
+// const Content = (props)=> {
+//   return (
+//     <div>
+//       <p>Content {props.part}</p>
+//     </div>
+//   )
+// }
+
+// const Total = (props)=> {
+//   return (
+//     <div>
+//       <p>TotalData {props.total}</p>
+//     </div>
+//   )
+// }
+// const App = () => {
+//   const course = 'Half Stack application development'
+//   const part1 = 'Fundamentals of React'
+//   const exercises1 = 10
+//   const part2 = 'Using props to pass data'
+//   const exercises2 = 7
+//   const part3 = 'State of a component'
+//   const exercises3 = 14
+
+//   return (
+//     <div>
+//      < Header course={course}/>
+//      < Content part={part1}/>
+//      < Content part={part2}/>
+//      < Content part={part3}/>
+//      < Total total={exercises1 + exercises2 + exercises3}/>
+
+//     </div>
+//   )
+// }
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{course}</h1>
+      <p>
+        {part1.name}
+      </p>
+      <p>
+        {part2.name}
+      </p>
+      <p>
+        {part3.name}
+      </p>
+      <p>
+        {part1.exercises}
+      </p>
+      <p>
+        {part2.exercises}
+      </p>
+      <p>
+        {part3.exercises}
+      </p>
     </div>
-  );
+  )
 }
+// import { useState } from "react";
 
-export default App;
+// const App = () => {
+//   const [counter, setCounter] = useState(0);
+
+//   setTimeout(() => setCounter(counter + 1), 1000);
+//   console.log("rendering...", counter);
+//   return <div>{counter}</div>;
+// };
+
+// export default App;
